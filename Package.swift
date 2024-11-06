ff3e018ca5d57df9226f2dbcd1242af0267ccdb4
Furkan <furkanalioglu00@gmail.com>
2024-11-06 17:17:16 +0300

    chore: test

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "VxHub",
-            targets: ["VxHub"]
+            targets: ["VxHub"] //
         ),
         .library(
             name: "VxHub-Appsflyer",
