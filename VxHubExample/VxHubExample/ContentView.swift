efbf861f5d71a1285b9f19d9cbe9023018a88119
Furkan Alioglu <91939823+furkanalioglu@users.noreply.github.com>
2024-11-06 17:06:13 +0300

    test

--- VxHubExample/VxHubExample/ContentView.swift
+++ VxHubExample/VxHubExample/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
+                .imageScale(.large) // nothing
                 .foregroundStyle(.tint)
         }
         .onTapGesture {
