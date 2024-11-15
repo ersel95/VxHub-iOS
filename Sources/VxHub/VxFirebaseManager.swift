a3f008844a383055d6810b4153e05d0b21098ac7
Furkan <furkanalioglu00@gmail.com>
2024-11-15 10:41:31 +0300

    log

--- Sources/VxHub/VxFirebaseManager.swift
+++ Sources/VxHub/VxFirebaseManager.swift
@@ -22,6 +22,7 @@ open class VxFirebaseManager: @unchecked Sendable {
             debugPrint("VXHUB: Failed to load Firebase configuration from \(path.path)") //TODO: - Move to logger
             return
         }
+        debugPrint("Firebase is \(FirebaseApp.app())")
         FirebaseApp.configure(options: options)
     }
 }
