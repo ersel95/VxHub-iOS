4276d1a316f4586409d6928780e8035fc0b328e0
Furkan <furkanalioglu@gmail.com>
2025-01-22 16:29:38 +0300

    feat: removed weaks

--- Sources/VxHub/Paywalls/VxPromoOffer/View/PromoOfferViewController.swift
+++ Sources/VxHub/Paywalls/VxPromoOffer/View/PromoOfferViewController.swift
@@ -37,11 +37,11 @@ final class PromoOfferViewController: VxNiblessViewController {
 extension PromoOfferViewController: PromoOfferViewModelDelegate {
     nonisolated func promoOfferDidClose() {
         debugPrint("Dismiss geldi 1")
-        DispatchQueue.main.async { [weak self] in
+        DispatchQueue.main.async {
             debugPrint("Dismiss geldi 2")
-            self?.dismiss(animated: true) {
+            self.dismiss(animated: true) {
                 debugPrint("Dismiss geldi 3")
-                self?.viewModel.onDismissWithoutPurchase?()
+                self.viewModel.onDismissWithoutPurchase?()
             }
         }
     }
