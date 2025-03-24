f9577a8910cbbdf271e96fc210af6d9a84ff0386
habip yesilyurt <habip.yesilyurt@volvoxmobile.com>
2025-03-24 08:53:46 +0300

    Firebase log

--- Sources/VxHub/VxHub.swift
+++ Sources/VxHub/VxHub.swift
@@ -1001,6 +1001,7 @@ private extension VxHub {
                 self.config?.responseQueue.async { [weak self] in
                     guard self != nil else { return }
                     if let url {
+                        VxLogger.shared.error("Error VxFirebaseManager configure")
                         VxFirebaseManager().configure(path: url)
                         Purchases.shared.attribution.setFirebaseAppInstanceID(VxFirebaseManager().appInstanceId)
                     }
