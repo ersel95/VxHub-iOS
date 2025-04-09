55dd914c0c29496d55f12dc05452916118e75cdf
furkanalioglu <furkanalioglu@gmail.com>
2025-04-09 09:34:34 +0300

    feat: rev log.

--- Sources/VxHub/Managers/VxRevenueCat.swift
+++ Sources/VxHub/Managers/VxRevenueCat.swift
@@ -75,6 +75,9 @@ internal final class VxRevenueCat: @unchecked Sendable {
                     let networkManager = VxNetworkManager()
                     networkManager.validatePurchase(transactionId: transaction?.transactionIdentifier ?? "COULD_NOT_FIND_TRANSACTION_ID")
                     completion?(true)
+                    debugPrint("Transaction is",transaction)
+                    debugPrint("Customer info is",customerInfo)
+                    debugPrint("Error is",error?.localizedDescription)
                 } else {
                     completion?(false)
                 }
