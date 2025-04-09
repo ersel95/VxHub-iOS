0b6a848dec5b4ad4009786d382b190d77a7da439
furkanalioglu <furkanalioglu@gmail.com>
2025-04-09 16:25:49 +0300

    Added collection cell.

---
+++ Sources/VxHub/Extensions/UICollectionView++.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  VxHub
+//
+//  Created by Furkan Alioğlu on 9.04.2025.
+//
+
+import UIKit
+
+extension UICollectionView {
+    
+    // MARK: - Register Cell
+    public func registerCell<T: UICollectionViewCell>(cellType: T.Type) {
+        let identifier = cellType.dequeuIdentifier
+        register(cellType, forCellWithReuseIdentifier: identifier)
+    }
+    
+    // MARK: - Dequeuing
+    public func dequeueReusableCell<T: UICollectionViewCell>(with type: T.Type, for indexPath: IndexPath) -> T {
+        return self.dequeueReusableCell(withReuseIdentifier: type.dequeuIdentifier, for: indexPath) as! T
+    }
+}
