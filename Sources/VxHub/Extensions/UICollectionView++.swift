20e79d5311cd250b34c967759859d5fa15d64e84
furkanalioglu <furkanalioglu@gmail.com>
2025-04-09 17:48:09 +0300

    feat: TODO: - Back to normal collecton

--- Sources/VxHub/Extensions/UICollectionView++.swift
+++ Sources/VxHub/Extensions/UICollectionView++.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-extension UICollectionView {
+public extension UICollectionView {
     
     // MARK: - Register Cell
     public func registerCell<T: UICollectionViewCell>(cellType: T.Type) {
